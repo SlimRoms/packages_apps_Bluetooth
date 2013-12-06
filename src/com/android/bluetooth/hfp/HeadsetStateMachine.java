7dc991e447dfc06030f1e5ccd7fc1c1ad2a41fea
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-12-06 03:18:48 +0000

    Merge tag 'android-4.4.1_r1' into HEAD
    Android 4.4.1 Release 1
    
    Conflicts:
    	src/com/android/bluetooth/a2dp/Avrcp.java
    	src/com/android/bluetooth/btservice/RemoteDevices.java
    	src/com/android/bluetooth/map/BluetoothMnsObexClient.java
    
    Change-Id: I28e8f1a0843aac8a2877e0b6c68ec5ecb5a86deb
    
    Conflicts:
    	src/com/android/bluetooth/map/BluetoothMnsObexClient.java

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -1260,8 +1260,7 @@ final class HeadsetStateMachine extends StateMachine {
             mVoiceRecognitionStarted + " mWaitingforVoiceRecognition: " + mWaitingForVoiceRecognition +
             " isInCall: " + isInCall());
         if (state == HeadsetHalConstants.VR_STATE_STARTED) {
-            if (!mVoiceRecognitionStarted &&
-                !isVirtualCallInProgress() &&
+            if (!isVirtualCallInProgress() &&
                 !isInCall())
             {
                 try {
