d85fc03f849dcea64f9606a5d31b7a6232b34a96
pramod kotreshappa <pramodk@codeaurora.org>
2014-03-03 13:04:11 -0800

    Bluetooth: Fix for BT fails to turn on in PDT test
    Fix for BT fails to turn on after several hours of PDT test
    Issue is due to disable timeout, host fails to get the wack ACK from
    the SoC during BT turn of. Adding ssr cleanup routine in disable
    timout routine
    
    CRs-Fixed: 614585
    
    Change-Id: Ia32da9426f976ddc71f0ad740312222b4b646c94

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1556,6 +1556,7 @@ public class AdapterService extends Service {
     private native static void classInitNative();
     private native boolean initNative();
     private native void cleanupNative();
+    /*package*/ native void ssrcleanupNative();
     /*package*/ native boolean enableNative();
     /*package*/ native boolean disableNative();
     /*package*/ native boolean setAdapterPropertyNative(int type, byte[] val);
